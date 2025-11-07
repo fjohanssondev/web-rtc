d4049ee498d96a4571e1f37a5443a0bc76172b82
Fredrik Johansson <fjohansson.creative@gmail.com>
2025-11-07 18:35:17 +0100

    feat: added more details

--- frontend/src/components/ui/video.tsx
+++ frontend/src/components/ui/video.tsx
@@ -1,27 +1,52 @@
+import clsx from "clsx"
+import type React from "react"
 import type { VideoHTMLAttributes } from "react"
+
+type Rounded = "sm" | "md" | "lg" | "xl" | "full"
 
 interface VideoProps extends VideoHTMLAttributes<HTMLVideoElement> {
   ref: React.RefObject<HTMLVideoElement | null>
+  className?: string
+  children: React.ReactNode
+  autoPlay?: boolean
+  rounded?: Rounded
 }
 
 interface VideoTitleProps {
   children: React.ReactNode
 }
 
-function Video({ ref, ...props }: VideoProps) {
+interface VideoUserDetailsProps {
+  role: string
+  name: string
+}
+
+function Video({ ref, className, children, autoPlay = true, rounded = "xl", ...props }: VideoProps) {
   return (
-    <div>
-      <video ref={ref} {...props} />
+    <div className="relative">
+      <video autoPlay={autoPlay} className={clsx(rounded ? `rounded-${rounded}` : "", "absolute object-cover", className)} ref={ref} {...props} />
+      <div className="p-4">
+        {children}
+      </div>
     </div>
   )
 }
 
 function VideoTitle({ children }: VideoTitleProps){
   return (
-    <div>
+    <div className="absolute text-6xl">
       {children}
     </div>
   )
 }
 
-export { Video, VideoTitle }
+function VideoUserDetails({ role, name }: VideoUserDetailsProps){
+  return (
+    <div className="flex flex-col">
+      <span className="text-white text-base">{role}</span>
+      <span className="text-white text-2xl font-medium">{name}</span>
+    </div>
+  )
+}
+
+export { Video, VideoTitle, VideoUserDetails }
