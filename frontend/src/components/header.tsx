7b69d0d2e9242518cc633d32d9314e5c503cf243
Fredrik Johansson <fjohansson.creative@gmail.com>
2025-11-08 01:39:03 +0100

    feat: added nav

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,14 +1,31 @@
 import { Link } from "@tanstack/react-router"
+import { Button } from "./ui/button"
 
-function Header(){
+function Header() {
   return (
-    <header>
-      <div className="p-2 flex gap-2">
-            <Link to="/" className="[&.active]:font-bold">
-              Home
-            </Link>{' '}
-          </div>
-          <hr />
+    <header className="border-b">
+      <div className="flex container items-center mx-auto py-4">
+        <div className="flex items-center">
+          <span className="font-medium">Video App</span>
+          <nav className="ml-24">
+            <ul className="flex space-x-2">
+              <li>
+                <Link to="/" className="[&.active]:font-medium [&.active]:underline hover:underline">
+                  Home
+                </Link>{' '}
+              </li>
+              <li>
+                <Link to="/meeting/create" className="[&.active]:font-medium [&.active]:underline hover:underline">
+                  Create meeting
+                </Link>{' '}
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="ml-auto">
+          <Button>Logout</Button>
+        </div>
+      </div>
     </header>
   )
 }
