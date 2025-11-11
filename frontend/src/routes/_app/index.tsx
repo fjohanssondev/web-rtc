046b89ca101bb879974dfb0232cd4f0d84cfacbb
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-11 19:20:15 +0100

    fix: changed route to login

--- frontend/src/routes/_app/index.tsx
+++ frontend/src/routes/_app/index.tsx
@@ -10,7 +10,7 @@ export const Route = createFileRoute("/_app/")({
 
     if (!data?.session) {
       throw redirect({
-        to: "/meeting/join",
+        to: "/login",
         search: {
           redirect: location.href,
         },
